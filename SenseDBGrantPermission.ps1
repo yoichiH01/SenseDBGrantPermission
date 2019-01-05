b939f21eb131c1dfde9352977db79993de75da75
yoichiH01 <46386743+yoichiH01@users.noreply.github.com>
2019-01-05 13:56:48 +1100

    Add files via upload

---
+++ SenseDBGrantPermission.ps1
@@ -0,0 +1,10 @@
+﻿# Qlik Sense DB - Grant Permission version 1.0 Author: Yoichi H #
+$PosgresLocation = "C:\Program Files\Qlik\Sense\Repository\PostgreSQL\9.6\bin"
+$PosgresLocation
+$sql = "C:\yheTemp\SenseDBGrantPermission.sql"
+$sql
+
+
+Start-Process -FilePath $("$PosgresLocation\psql.exe") -ArgumentList "-h localhost -p 4432 -d QSR -U postgres -f $sql"
+
+Write-Host "Script execution Finished `r`n"
---
+++ SenseDBGrantPermission.sql
@@ -0,0 +1,3 @@
+/*#Qlik Sense DB - Grant Permission version 1.0 Author: Yoichi H*/
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO postgres;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO qliksenserepository;
